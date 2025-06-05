d0aaa7fdcd3c6897d31cac8e9ea304fd24ec57c1
aid003 <teremochek859@gmail.com>
2025-06-05 21:19:08 +0300

    init

---
+++ src/background.ts
@@ -0,0 +1,13 @@
+import browser from "webextension-polyfill";
+
+console.log("Hello from the background!");
+
+browser.runtime.onInstalled.addListener((details) => {
+  console.log("Extension installed:", details);
+  
+  if (details.reason === "install" || details.reason === "update") {
+    browser.tabs.create({
+      url: browser.runtime.getURL("/welcome.html")
+    });
+  }
+});
